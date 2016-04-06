444a46ed83406a4cf68b427deacbd6975825d85d
Mikael Brosset <mikael.brosset@gmail.com>
2016-04-06 11:37:42 +0200

    readme file added

---
+++ app/parameters.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by Mikaël Brosset.
+ * Email: m.brosset@darkmira.com
+ */
+
+//CSV
+define('ARGFILE','f');
+define('ARGSEPARATOR','s');
+define('CSVPATH','../Resources/import.csv');
+define('CSVSEPARATOR', ',');
+
+
+//SWIFTMAILER
+define('SUBJECT', 'Subject');
+define('FROM', 'darkmira@darkmira.com');
+define('TRANSPORT', 'darkmira.com');
+define('TRANSPORT_USERNAME', 'username');
+define('TRANSPORT_PASSWORD', 'password');
+
+//DB
+define('DBUSERNAME', 'root');
+define('DBPASSWORD', 'pich');
+define('DBHOST', 'localhost');
+
