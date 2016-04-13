5e9f3247b9e98b8f9fe4082dc1e55838867cbfb4
Mikael Brosset <mikael.brosset@gmail.com>
2016-04-13 13:15:40 +0200

    error displey removed

--- src/Command/CSVToMailCommand.php
+++ src/Command/CSVToMailCommand.php
@@ -3,8 +3,7 @@
  * Created by Mikaël Brosset.
  * Email: m.brosset@darkmira.com
  */
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
 include '../../autoload.php';
 include '../../app/parameters.php';
 require_once '../../vendor/swiftmailer-5.x/lib/swift_required.php';
