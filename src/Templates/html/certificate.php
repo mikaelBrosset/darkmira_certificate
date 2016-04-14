fe96954e89d50344c85e6f7d14ecfed301b6a143
Mikael Brosset <mikael.brosset@gmail.com>
2016-04-14 11:49:36 +0200

    signatures added

--- src/Templates/html/certificate.php
+++ src/Templates/html/certificate.php
@@ -8,37 +8,43 @@
     </head>
     <body>
 
-    <div class="mainWrapper">
-        <h1>CERTIFICADO</h1>
-        <p class="secondaryText">
-            Certificamos que <span class="smallTitle"><?php echo $alumni->getFirstname().' '. $alumni->getLastName() ?></span> participou do <span class="smallTitle">DARKMIRA Tour PHP 2016 </span> ocorrido nos dias 19 e 20 de Março 2016, na Universidade Católica de Brasília.
+        <div class="mainWrapper">
+            <div class="mainTextWrapper">
+                <h1>CERTIFICADO</h1>
+                <p class="secondaryText">
+                    Certificamos que <span class="smallTitle"><?php echo $alumni->getFirstname().' '. $alumni->getLastName() ?></span> participou do <span class="smallTitle">DARKMIRA Tour PHP 2016 </span> ocorrido nos dias 19 e 20 de Março 2016, na Universidade Católica de Brasília.
 
-        </p>
-        <p class="tertiaryText">
-            Carga horària de 8 horas.
-        </p>
+                </p>
+                <p class="tertiaryText">
+                    Carga horària de 8 horas.
+                </p>
 
-        <div class="signatureWrap">
-            <div class="signatureBlock">
-                <!--<img src="../web/media/CGL-signature_alpha_white.png">-->
-                <div class="imageWrap"></div>
-                <hr>
-                <div class="signatureNames">
-                    Frédéric BAILLON<br>
-                    CEO Darkmira
-                </div>
-            </div>
+                <div class="signatureWrap">
+                    <div class="signatureBlock">
+                        <!--<img src="../web/media/CGL-signature_alpha_white.png">-->
+                        <div class="imageWrap">
+                            <img src="../web/media/FBA_signature.png">
+                        </div>
+                        <div class="stroke"></div>
+                        <div class="signatureNames">
+                            Frédéric BAILLON<br>
+                            CEO Darkmira
+                        </div>
+                    </div>
 
 
-            <div class="signatureBlock">
-                <div class="imageWrap"></div>
-                <hr>
-                <div class="signatureNames">
-                    Cyrille GRANDVAL<br>
-                    CEO Darkmira Brazil
+                    <div class="signatureBlock">
+                        <div class="imageWrap">
+                            <img src="../web/media/CGL_signature.png">
+                        </div>
+                        <div class="stroke"></div>
+                        <div class="signatureNames">
+                            Cyrille GRANDVAL<br>
+                            CEO Darkmira Brazil
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     </body>
 </html>
