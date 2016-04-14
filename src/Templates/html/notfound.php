cfb955ded7c6153fab4cb87457072e8daf0145c1
Mikael Brosset <mikael.brosset@gmail.com>
2016-04-14 12:25:31 +0200

    form and not found page style

--- src/Templates/html/notfound.php
+++ src/Templates/html/notfound.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Darkmira Certficate</title>
-        <link rel="stylesheet" href="web/css/certificate.css"
-    </head>
-    <body>
-    <div class="darkmiraImage"></div>
-    <div class="mainWrapper">
-        <h2>ALUMNI not found !!!</h2>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Darkmira Form</title>
+    <link rel="stylesheet" href="../web/css/form.css"
+</head>
+<body>
+<div class="mainWrapper">
+    <h2>
+        Por favor, digite o número que você recebeu por email
+    </h2>
+    <div class="formWrapper">
+        <p class="notFoundtext">NÃO ENCONTRADO !!!<br>
+            Tente outro
+        </p>
+        <form method="GET" action="alumni.php">
+            <div class="inputTextWrapper">
+                <input type="text" name="nb" class="inputText"></br>
+            </div>
+            <div class="submitWrapper">
+                <input type="submit" class="validButton">
+            </div>
+        </form>
     </div>
-    </body>
+</div>
+</body>
 </html>
